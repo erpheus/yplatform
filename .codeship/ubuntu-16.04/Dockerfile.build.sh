b087a6218aae1eb871bd3e26eecd378df3690ba5
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-29 16:41:05 +0100

    Revert "debug"
    This reverts commit f6b7fdcb8b78162a4285d04effd48f4b0cddae9f.

--- .codeship/ubuntu-16.04/Dockerfile.build.sh
+++ .codeship/ubuntu-16.04/Dockerfile.build.sh
@@ -43,5 +43,4 @@ GIT_USER_NAME="Codeship"
 source ${YP_DIR}/dockerfiles/util/bootstrap.inc.sh
 source ${YP_DIR}/dockerfiles/util/clean.inc.sh
 
-echo UNAME=${UNAME} /Dockerfile.test.sh
 UNAME=${UNAME} /Dockerfile.test.sh
